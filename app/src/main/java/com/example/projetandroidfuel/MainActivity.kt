fe9cb7e392ced7597f3fa6f3fab869e923417271
LeonFuchs <leonfuchs2003@gmail.com>
2024-12-09 17:35:43 +0100

    Initial Map implementation

--- app/src/main/java/com/example/projetandroidfuel/MainActivity.kt
+++ app/src/main/java/com/example/projetandroidfuel/MainActivity.kt
@@ -4,22 +4,32 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 const val SERVER_BASE_URL = "https://app-29cf3f13-cafb-432e-9b23-f0010d073522.cleverapps.io/"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private val stations = StationHM()
 
     private val retrofit = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
         .baseUrl(SERVER_BASE_URL).build()
     private val stationService = retrofit.create(StationService::class.java)
+
+    private val supportMapFragment = SupportMapFragment.newInstance()
 
     //TODO Figure where to use startForResult (maybe in another file)
     private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -40,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                 call: Call<List<Station>>, response: Response<List<Station>>
             ) {
                 val allBooks: List<Station>? = response.body()
-                allBooks?.forEach { stations.addBook(it) }
+                allBooks?.forEach { stations.addStation(it) }
                 displayStationListFragment()
             }
 
@@ -63,6 +73,13 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 
+    private fun displayMapFragment() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.a_main_lyt_container, SupportMapFragment())
+        fragmentTransaction.commit()
+        supportMapFragment.getMapAsync(this)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_delete -> {
@@ -71,6 +88,27 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        //TODO remove gardanne
+        val gardanne = LatLng(43.450001,5.466667)
+        map.addMarker(
+            MarkerOptions()
+            .position(gardanne)
+            .title("Gardanne")
+            .snippet("ISMIN")
+        )
+        map.moveCamera(CameraUpdateFactory.zoomTo(4F))
+        map.moveCamera(CameraUpdateFactory.newLatLng(gardanne))
+        stations.getAllStations().forEach{
+            map.addMarker(
+                MarkerOptions()
+                    .position(LatLng(it.latitude.toDouble()/100000,it.longitude.toDouble()/100000))
+                    .title(it.carburants.joinToString(","))
+                    .snippet(it.toSnippet())
+            )
         }
     }
 }
--- app/src/main/java/com/example/projetandroidfuel/Station.kt
+++ app/src/main/java/com/example/projetandroidfuel/Station.kt
@@ -6,10 +6,19 @@ data class Station (
     val id: Int,
     val latitude: Int,
     val longitude: Int,
-    val cp: Int,
+    val cp: String,
     val adresse: String,
     val ville: String,
     val prix: List<PrixElement>,
     val carburants: List<String>,
     val favorite: Boolean
 ) : Serializable
+{
+    fun toSnippet(): String {
+        var res = ""
+        this.prix.forEach {
+            res += it.nom + " : " + it.valeur + "\n"
+        }
+        return res
+    }
+}
